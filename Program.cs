b19b810d21961eba7bd9097db5923e525f6085b2
eduardopires <eduardopires@concilig.com.br>
2021-08-27 08:55:50 -0300

    Criação da classe Posição

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
+using tabuleiro;
+
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,27 @@
+﻿namespace tabuleiro
+{
+    class Posicao
+    {
+        #region Propriedades
+
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        #endregion
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        #region Métodos
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
+
+        #endregion
+    }
+}
