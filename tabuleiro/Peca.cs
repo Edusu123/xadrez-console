0ada18934b5d973a7a6de3d513ee7fb812dd44d5
eduardopires <eduardopires@concilig.com.br>
2021-08-30 08:32:08 -0300

    Começando a implementar o tabuleiro

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        #region Propriedades
+
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; set; }
+
+        #endregion
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            Posicao = posicao;
+            Tab = tab;
+            Cor = cor;
+            QtdMovimentos = 0;
+        }
+    }
+}
---
+++ tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        #region Propriedades
+
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas { get; set; }
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
+
+        #endregion
+
+
+    }
+}
