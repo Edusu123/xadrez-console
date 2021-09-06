e6330509e03b816a0fa82c965226d198642b8177
eduardopires <eduardo.c.pires@outlook.com>
2021-09-06 08:50:40 -0300

    Implementação da peça Bispo

---
+++ xadrez/Bispo.cs
@@ -0,0 +1,85 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Bispo : Peca
+    {
+        public Bispo(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor) { }
+
+        #region Métodos
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            var mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Noroeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                    break;
+
+                pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            }
+
+            // Nordeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                    break;
+
+                pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            }
+
+            // Sudeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                    break;
+
+                pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            }
+
+            // Sudoeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                    break;
+
+                pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            }
+
+            return mat;
+        }
+
+        #endregion
+
+        #region Funções
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        #endregion
+    }
+}
