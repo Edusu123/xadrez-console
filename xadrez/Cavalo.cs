00620167d753d18ccc614c046201741dcb69c451
eduardopires <eduardo.c.pires@outlook.com>
2021-09-06 08:58:05 -0300

    Implementação da peça cavalo

---
+++ xadrez/Cavalo.cs
@@ -0,0 +1,69 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Cavalo : Peca
+    {
+        public Cavalo(Tabuleiro tab, Cor cor) : base(tab, cor) { }
+
+        #region Métodos
+
+        public override string ToString()
+        {
+            return "C";
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            var mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            return mat;
+        }
+
+        #endregion
+
+        #region Funções
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        #endregion
+    }
+}
